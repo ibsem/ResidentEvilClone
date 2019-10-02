c5f8da2c7e3e8fdfb1f6b864021b71ccd3ff7fee
teago83 <supremeleaderpastel@gmail.com>
2019-10-02 14:42:46 -0300

    Some adjustments made to the overall lighting and added the tank controls script to the character.

---
+++ ResidentEvilClone/Assets/Scripts/Character/TankControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public bool isMoving;
+    public float horizontalMove;
+    public float verticalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            isMoving = true;
+            thePlayer.GetComponent<Animator>().Play("Walk");
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 4;
+            thePlayer.transform.Rotate(0, horizontalMove, 0);
+            thePlayer.transform.Translate(0, 0, verticalMove);
+        }
+        else
+        {
+            isMoving = false;
+            thePlayer.GetComponent<Animator>().Play("Idle");
+        }
+    }
+}
