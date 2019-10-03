d624d10e79889412fedf12485eedee0231e2fd2e
teago83 <supremeleaderpastel@gmail.com>
2019-10-03 13:43:35 -0300

    Added cameras, camera triggers, a box collider to the player and a way of climbing the stairs without getting stuck or completely traversing them.

---
+++ ResidentEvilClone/Assets/Scripts/Cameras/CameraSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject CameraActivate;
+    public GameObject CameraDeactivate;
+
+    void OnTriggerEnter(Collider other){
+        if (other.tag == "Player"){
+            CameraActivate.SetActive(true);
+            CameraDeactivate.SetActive(false);
+        }
+    }
+}
